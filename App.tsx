a052a6c645743ac5be10c61ef6a11a9459f5de37
pavelchlebek <pavelchlebek990@gmail.com>
2022-06-15 09:58:05 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+import * as Location from 'expo-location';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+type TErrorMessage = string | undefined
+
+type TLocation = Location.LocationObject | undefined
 
 export default function App() {
+  const [location, setLocation] = React.useState<TLocation>()
+  const [errorMsg, setErrorMsg] = React.useState<TErrorMessage>()
+
+  React.useEffect(() => {
+    const getLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied")
+        return
+      }
+
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+    }
+    getLocation()
+  }, [])
+
+  let text = "Waiting.."
+  if (errorMsg) {
+    text = errorMsg
+  } else if (location) {
+    text = JSON.stringify(location)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>{text}</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+  text: {
+    fontSize: 20,
+  },
+})
