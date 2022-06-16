a4ac00724a4947bfbfccd06d990fe9c77bb6d44c
pavelchlebek <pavelchlebek990@gmail.com>
2022-06-16 07:15:18 +0200

    Getting coords by clicking the button, storing positions, calculating total distance

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 import * as Location from 'expo-location';
+import { StatusBar } from 'expo-status-bar';
 import {
+  Button,
+  ScrollView,
   StyleSheet,
   Text,
   View,
@@ -9,36 +12,78 @@ import {
 
 type TErrorMessage = string | undefined
 
-type TLocation = Location.LocationObject | undefined
+type TLocation = {
+  lat: number
+  long: number
+}
+
+const calculateDistance = (prevPosition: TLocation, currentPosition: TLocation) => {
+  return Math.sqrt(
+    (prevPosition.lat - currentPosition.lat) ** 2 + (prevPosition.long - currentPosition.long) ** 2
+  )
+}
 
 export default function App() {
-  const [location, setLocation] = React.useState<TLocation>()
+  const [coords, setCoords] = React.useState<TLocation>()
   const [errorMsg, setErrorMsg] = React.useState<TErrorMessage>()
 
-  React.useEffect(() => {
-    const getLocation = async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync()
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied")
-        return
-      }
+  const [positions, setPositions] = React.useState<TLocation[]>([])
+  const [distance, setDistance] = React.useState(0)
 
-      let location = await Location.getCurrentPositionAsync({})
-      setLocation(location)
+  const getLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync()
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied")
+      return
     }
-    getLocation()
-  }, [])
 
-  let text = "Waiting.."
-  if (errorMsg) {
-    text = errorMsg
-  } else if (location) {
-    text = JSON.stringify(location)
+    let location = await Location.getCurrentPositionAsync({})
+    const currentCoords = { lat: location.coords.latitude, long: location.coords.longitude }
+    setCoords(currentCoords)
+    setPositions([...positions, currentCoords])
+    if (positions.length > 1) {
+      const currentDistanceDelta = calculateDistance(
+        positions[positions.length - 2],
+        positions[positions.length - 1]
+      )
+      setDistance((prev) => prev + currentDistanceDelta)
+    }
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{text}</Text>
+      <StatusBar style="auto" />
+      <View style={styles.coords}>
+        <View style={styles.data}>
+          <Text style={styles.label}>Latitude:</Text>
+          <Text style={styles.value}>{coords?.lat}</Text>
+        </View>
+        <View style={styles.data}>
+          <Text style={styles.label}>Longitude:</Text>
+          <Text style={styles.value}>{coords?.long}</Text>
+        </View>
+      </View>
+      <Button onPress={getLocation} title="Get Coords" />
+      <View style={styles.data}>
+        <Text style={styles.label}>Distance:</Text>
+        <Text style={styles.value}>{(distance * 111111.111).toFixed(2)} meters</Text>
+      </View>
+      <ScrollView>
+        {positions.map((position, index) => {
+          return (
+            <View key={index} style={styles.coords}>
+              <View style={styles.data}>
+                <Text style={styles.label}>Latitude:</Text>
+                <Text style={styles.value}>{position.lat}</Text>
+              </View>
+              <View style={styles.data}>
+                <Text style={styles.label}>Longitude:</Text>
+                <Text style={styles.value}>{position.long}</Text>
+              </View>
+            </View>
+          )
+        })}
+      </ScrollView>
     </View>
   )
 }
@@ -49,8 +94,21 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    marginTop: 50,
   },
-  text: {
+  coords: {
+    marginBottom: 30,
+  },
+  data: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  label: {
+    fontSize: 16,
+    marginRight: 30,
+  },
+  value: {
     fontSize: 20,
   },
 })
